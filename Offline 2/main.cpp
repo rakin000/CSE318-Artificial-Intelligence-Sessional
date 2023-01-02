bccd9ced3efec72c1e7e0edb0aab18093726f6d6
Aszad Rakin <rakinkzs@gmail.com>
2023-01-02 10:59:24 +0000

    offline 2

---
+++ Offline 2/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+
+int n; 
+vector<vector<int>> row,col,matrix; 
+
+void backtrack(int r, vector<vector<int>> &matrix){
+    if( r==n ){
+        return ;
+    }
+
+    for()
+}
+
+
+void runner(){
+
+}
+
+int main(){
+    ios_base::sync_with_stdio(false) ;
+    cout<<"HOWDY\n"<<endl;
+}
